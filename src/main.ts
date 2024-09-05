88f94c1c2407eff06243cc33b391c762fd1d1897
RyAsDeveloper <itikawa.genki.hidaka@outlook.jp>
2024-09-05 14:37:13 +0900

    deploy 2024/09/05 14:36

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import './style.css'
+import './assets/font.css'
+
+
+import App from './App.vue'
+
+
+
+
+const app = createApp(App);
+app.mount('#app');
---
+++ src/menuitem.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+defineProps<{content: String}>();
+</script>
+
+<style scoped>
+
+</style>
